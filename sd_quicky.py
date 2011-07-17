72c80e8ced4f615f45e7096489e2760f8fbe063b
Brian Sierakowski <briansierakowski@gmail.com>
2011-07-17 10:05:12 -0400

    cut out matchOrDie as i'm using inline if's and findall in parseEntry(). Probably a bad idea

--- sd_quicky.py
+++ sd_quicky.py
@@ -10,17 +10,6 @@ BITLY_LOGIN="sdbaltimore"
 BITLY_API="R_3677d6826fab742f02f027226dff3d2c"
 BITLY=bitly.Api(login=BITLY_LOGIN,apikey=BITLY_API)
 
-def matchOrEmpty(regex,text):
-    match=regex.findall(text)
-    text=match[0] if match else ''
-
-    #remove any br tags
-    text=re.compile("</?\w+\s*/?>").sub("",text)
-
-    return text.strip()
-
-
-
 def parseEntry(entry):
     d={}
     d["title"]=entry.title.text
@@ -41,21 +30,6 @@ def parseEntry(entry):
     for link in link_regex:
         d["short-link"].append(BITLY.shorten(link))
     return d
-        
-        
-      #  try:
-           # d["link"]= matchOrEmpty(re.compile("http://[^ \s]+"),content) or matchOrEmpty(re.compile("www.[^ \s]+"),content)
-
-            #try to shorten link, fall back to regular link
-           # d["short-link"]=d["link"]
-           # try:
-           #     d["short-link"]=BITLY.shorten(d["link"])
-           # except:
-            #    pass
-       # except:
-            #if we don't find one, go on
-            #pass
-    # return d
 
 def monday2monday(near):
     #gcal api uses inclusive start date, exclusive end date
